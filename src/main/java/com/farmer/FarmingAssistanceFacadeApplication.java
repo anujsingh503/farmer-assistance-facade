849a854a1fdc112d9dc1fb5b558bcff400613469
Anuj Singh <anujs503@gmail.com>
2021-08-19 00:26:13 +0100

    Project initialized

---
+++ src/main/java/com/farmer/FarmingAssistanceFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.farmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingAssistanceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmingAssistanceFacadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facade/FarmingAssistanceFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingAssistanceFacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
